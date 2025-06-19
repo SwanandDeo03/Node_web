a6f435ae8f80e803dea8713130b11ff61c420111
SwanandDeo03 <swananddeo3@gmail.com>
2025-06-19 13:52:07 +0530

    updated server.js file

--- server.js
+++ server.js
@@ -21,6 +21,6 @@ app.post('/contact', (req, res) => {
     res.send('<h2>Thank you for contacting us!</h2><p>We have received your message and will get back to you soon.</p><a href="/">Back to Home</a>');
 });
 
-app.listen(PORT, () => {
-    console.log(`Server running at http://localhost:${PORT}`);
-}); 
+app.listen(3000, '0.0.0.0', () => {
+  console.log("Server running at http://0.0.0.0:3000");
+});
